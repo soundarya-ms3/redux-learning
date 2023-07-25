62350702c58bd5f5b158bc4167e31216766e8646
soundarya-ms3 <soundaryams03@gmail.com>
2023-07-25 17:05:07 +0530

    Multiple reducers & CombineReducers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import redux from 'redux';
 const redux = require('redux')
 const createStore= redux.createStore
+const combineReducers=redux.combineReducers
 
 const BUY_CAKE ='BUY_CAKE'
 const BUY_ICECREAM='BUY_ICECREAM'
@@ -18,17 +19,47 @@ function buyIcecream(){
     }
 }
 
-const initialState = {
-    numOfCakes: 10,
-    numOfIcecreams:10,
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIcecreams:10,
+// }
+
+const initialCakeState={
+     numOfCakes: 10,
+}
+const initialIcecreamState= {
+    numOfIcecreams: 20,
 }
 
-const reducer = (state=initialState,action) =>{
+// const reducer = (state=initialState,action) =>{
+//     switch(action.type){
+//         case BUY_CAKE: return{
+//             ...state,
+//             numOfCakes:state.numOfCakes-1
+//         }
+//         case BUY_ICECREAM: return{
+//             ...state,
+//             numOfIcecreams:state.numOfIcecreams-1
+//         }
+//         default: return state
+            
+//     }
+// }
+
+
+const cakeReducer = (state=initialCakeState,action) =>{
     switch(action.type){
         case BUY_CAKE: return{
             ...state,
             numOfCakes:state.numOfCakes-1
         }
+        default: return state
+            
+    }
+}
+
+const iceCreamreducer = (state=initialIcecreamState,action) =>{
+    switch(action.type){
         case BUY_ICECREAM: return{
             ...state,
             numOfIcecreams:state.numOfIcecreams-1
@@ -37,8 +68,13 @@ const reducer = (state=initialState,action) =>{
             
     }
 }
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream:iceCreamreducer
+}
+)
 
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 console.log('initial state',store.getState())
 const unsubscribe = store.subscribe(() => console.log('updated state',store.getState()))
 store.dispatch(buyCake())
